5d56b81fcb10927c1914d8c2406d048f68424d37
alinamz <anika20099@yandex.ru>
2023-03-25 15:58:05 +0400

    Change Router to HashRouter

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,10 @@ import '../index.css';
 import Search from '../Search/Search';
 import CardList from '../Components/CardList/CardList';
 import { Routes, Route } from "react-router-dom";
-import { ProtectedRoute } from '../Components/ProtectedRoute'
 import { useState } from 'react';
 import CardDetals from '../Components/CardDetals/CardDetals';
 
 function App() {
-  const [loggedIn] = useState(true);
 
   const [card, setCard] = useState({});
   const [search, setSearch] = useState('');
@@ -16,17 +14,17 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={
-          <ProtectedRoute loggedIn={loggedIn} >
+          <div>
             <Search search={search} setSearch={setSearch}></Search>
             <CardList setCard={setCard}></CardList>
-          </ProtectedRoute>
+          </div>
         } />
 
         <Route path='/card' element={
-          <ProtectedRoute loggedIn={loggedIn} >
+          <div>
              <Search search={search}></Search>
              <CardDetals card={card}></CardDetals>
-          </ProtectedRoute>
+          </div>
         } />
      </Routes>
     </div>
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 
 export default function Card({ card, setCard }) {
-    
-   function onclick() {
-    setCard(card)
-   }
+
+    function onclick() {
+        setCard(card);
+    }
 
     return (
         <Link className="card" to='/card' onClick={onclick}>
-                { card.img == null || card.img === '' ? <p className="card__img-none">Без обложки</p> : <img className="card__image" alt={card.title} src={card.img} /> }
-                <div className="card__info">
-                    {card?.categories == null || card.categories.length === 0 ? '' : <p className="card__categories">{ card.categories[0] }</p>}
-                    <h2 className="card__name">{card.title}</h2>
-                    <p className="card__author">{card.authors}</p>
-                </div>
+            {card.img == null || card.img === '' ? <p className="card__img-none">Без обложки</p> : <img className="card__image" alt={card.title} src={card.img} />}
+            <div className="card__info">
+                {card?.categories == null || card.categories.length === 0 ? '' : <p className="card__categories">{card.categories[0]}</p>}
+                <h2 className="card__name">{card.title}</h2>
+                <p className="card__author">{card.authors}</p>
+            </div>
         </Link >
     )
 }
